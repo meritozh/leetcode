7afe31fffc39e01ffdf0557b96d23b368790da68
Gao Wanqiu <wanqiu.gao@ele.me>
2018-09-12 11:56:21 +0800

    rename, add linked list problems

---
+++ cpp/LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,6 @@
+/**
+ * Description:
+ *
+ * Reverse a singly linked list.
+ */
+
--- cpp/array/ContainsDuplicate2.cpp
+++ cpp/array/ContainsDuplicate2.cpp
@@ -7,14 +7,34 @@
  */
 
 #include <vector>
+#include <map>
 
 class Solution {
 public:
   bool ContainsDuplicate(std::vector<int>& Nums, int K) {
+    // <value, index>
+    std::map<int, int> M;
 
+    auto End = Nums.end();
+    for (int I = 0, Max = Nums.size(); I < Max; ++I) {
+      auto E = Nums[I];
+      auto Iter = M.find(E);
+      if (Iter == M.end()) {
+        M.insert({E, I});
+      } else {
+        if ((I - Iter->second) <= K) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 };
 
 int main() {
+  std::vector<int> In = {1, 2, 6, 7, 1, 3, 2, 4};
+  assert(Solution().ContainsDuplicate(In, 4) == true);
 
+  return 0;
 }
